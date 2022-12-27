02d93d29185bd20300d2693a50a64c9f1b1cdd38
Max Thanachai Thongkum <singmax161@gmail.com>
2022-12-27 23:24:53 +0700

    add detail in /

--- src/main/kotlin/th/nstda/thongkum/tele_api/plugins/Routing.kt
+++ src/main/kotlin/th/nstda/thongkum/tele_api/plugins/Routing.kt
@@ -3,14 +3,33 @@ package th.nstda.thongkum.tele_api.plugins
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import th.nstda.thongkum.tele_api.getLogger
 
 fun Application.configureRouting() {
-
     routing {
         get("/") {
             getLogger(this::class.java).info("ทดสอบไทย")
-            call.respondText("Hello World!")
+            val response = Detail(
+                "Apache License 2.0",
+                "http://www.apache.org/licenses/",
+                "https://lionants02.github.io/tele_api/",
+                arrayOf(Contributor("Thanachai Thongkum", "https://github.com/lionants02"))
+            )
+            call.respond(response)
         }
+
     }
 }
+
+@Serializable
+internal data class Contributor(val name: String, val url: String)
+
+@Serializable
+internal data class Detail(
+    val license: String,
+    val licenseUrl: String,
+    val docUrl:String,
+    val developers: Array<Contributor>
+)
